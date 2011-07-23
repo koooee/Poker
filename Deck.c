319682778c4a749397788cc54eea37a94b597e91
Nick Kolegraff <nick@winthropgaming.com>
2011-07-23 01:11:53 -0400

    added Deck.c...for some reason stopped tracking it

---
+++ Deck.c
@@ -0,0 +1,14 @@
+#include "Deck.h"
+
+void InitDeck(void)
+{
+  int i,j;
+  for(i = 0; i < MAX_NUM_RANKS; i++)
+    for(j = 0; j < MAX_NUM_SUITS; j++)
+      {
+	int di = (i * (MAX_NUM_SUITS-1)) + i+j; /* Deck Index */
+	deck[di].rank = i;
+	deck[di].suit = j;
+	deck[di].whos_card = 'N';
+      }
+}
