04cb0370edc20f9c30f96e13335b9399a16b7ae8
Nick Kolegraff <nick@winthropgaming.com>
2011-10-24 05:01:05 -0500

    Testing Simulation

--- Sim.c
+++ Sim.c
@@ -8,6 +8,7 @@
 #include "Test.h"
 
 void Simulate();
+void jtest();
 void itcard(int icard, CARD *c);
 
 int main(int argc, char *argv[])
@@ -20,13 +21,50 @@ int main(int argc, char *argv[])
   /* seed = atoi(argv[1]); */
   /* ttest(seed); */
   /* End Debugging */
-  Simulate();
+  /* Simulate(); */
+  jtest();
 }
 
 void itcard(int icard, CARD *c)
 {
   c->rank = icard % 13;
   c->suit = icard % 4;
+}
+
+void jtest()
+{
+  unsigned long long totalcombin = 0;
+  int action, holep0, holep1, holed0, holed1, flop0, flop1, flop2, turn, river;
+  int decksize = 52;
+  for(holep0 = 0; holep0 < decksize; holep0++){
+    for(holep1 = holep0+1; holep1 < decksize; holep1++){
+      for(holed0 = 0; holed0 < decksize; holed0++){
+	for(holed1 = holed0+1; holed1 < decksize; holed1++){
+	  for(flop0 = 0; flop0 < decksize; flop0++){
+	    for(flop1 = flop0+1; flop1 < decksize; flop1++){
+	      for(flop2 = flop1+1; flop2 < decksize; flop2++){
+		for(turn = 0; turn < decksize; turn++){
+		  for(river = turn+1; river < decksize; river++){
+		    if(holep0 != holep1 && holep0 != holed0 && holep0 != holed1 && holep0 != flop0 && holep0 != flop1 && holep0 != flop2 && holep0 != turn && holep0 != river
+		       && holep1 != holed0 && holep1 != holed1 && holep1 != flop0 && holep1 != flop1 && holep1 != flop2 && holep1 != turn && holep1 != river
+		       && holed0 != holed1 && holed0 != flop0 && holed0 != flop1 && holed0 != flop2 && holed0 != turn && holed0 != river
+		       && holed1 != flop0 && holed1 != flop1 && holed1 != flop2 && holed1 != turn && holed1 != river
+		       && flop0 != flop1 && flop0 != flop2 && flop0 != turn && flop0 != river
+		       && flop1 != flop2 && flop1 != turn && flop1 != river
+		       && flop2 != turn && flop2 != river
+		       && turn != river){
+		      totalcombin++;
+		    }
+		  }
+		}
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  }
+  printf("Final: lld\n", totalcombin);
 }
 
 void Simulate()
@@ -52,14 +90,14 @@ void Simulate()
   char *state = malloc(2 * sizeof(char));
   char *is_my_card;
   for(holep0 = 0; holep0 < decksize; holep0++){
-    for(holep1 = 0; holep1 < decksize; holep1++){
+    for(holep1 = holep0+1; holep1 < decksize; holep1++){
       for(holed0 = 0; holed0 < decksize; holed0++){
-	for(holed1 = 0; holed1 < decksize; holed1++){
+	for(holed1 = holed0+1; holed1 < decksize; holed1++){
 	  for(flop0 = 0; flop0 < decksize; flop0++){
-	    for(flop1 = 0; flop1 < decksize; flop1++){
-	      for(flop2 = 0; flop2 < decksize; flop2++){
+	    for(flop1 = flop0+1; flop1 < decksize; flop1++){
+	      for(flop2 = flop1+1; flop2 < decksize; flop2++){
 		for(turn = 0; turn < decksize; turn++){
-		  for(river = 0; river < decksize; river++){
+		  for(river = turn+1; river < decksize; river++){
 		    if(holep0 != holep1 && holep0 != holed0 && holep0 != holed1 && holep0 != flop0 && holep0 != flop1 && holep0 != flop2 && holep0 != turn && holep0 != river
 		       && holep1 != holed0 && holep1 != holed1 && holep1 != flop0 && holep1 != flop1 && holep1 != flop2 && holep1 != turn && holep1 != river
 		       && holed0 != holed1 && holed0 != flop0 && holed0 != flop1 && holed0 != flop2 && holed0 != turn && holed0 != river
@@ -69,68 +107,68 @@ void Simulate()
 		       && flop2 != turn && flop2 != river
 		       && turn != river){
 		      totalCombin++;
+		      
+		      /* // There are four actions described below */
+		      /* // Bet 4x                  0 */
+		      /* // Check First, Bet 2x     1 */
+		      /* // Check, Check, Bet 1x    2 */
+		      /* // Check, Check, Fold      3 */
 
-		      // There are four actions described below
-		      // Bet 4x                  0
-		      // Check First, Bet 2x     1
-		      // Check, Check, Bet 1x    2
-		      // Check, Check, Fold      3
-
-		      // Players Hole Cards
-		      itcard(holep0, &p.hand[0]);
-		      itcard(holep1, &p.hand[1]);
-		      p.hand[0].whos_card = PLAYERS;
-		      p.hand[1].whos_card = PLAYERS;
+		      /* // Players Hole Cards */
+		      /* itcard(holep0, &p.hand[0]); */
+		      /* itcard(holep1, &p.hand[1]); */
+		      /* p.hand[0].whos_card = PLAYERS; */
+		      /* p.hand[1].whos_card = PLAYERS; */
 			  
-		      // Dealers Hole Cards
-		      itcard(holed0, &d.hand[0]);
-		      itcard(holed1, &d.hand[1]);
-		      d.hand[0].whos_card = DEALERS;
-		      d.hand[0].whos_card = DEALERS;
+		      /* // Dealers Hole Cards */
+		      /* itcard(holed0, &d.hand[0]); */
+		      /* itcard(holed1, &d.hand[1]); */
+		      /* d.hand[0].whos_card = DEALERS; */
+		      /* d.hand[1].whos_card = DEALERS; */
 
-		      // Flop
-		      itcard(flop0, &p.hand[2]);
-		      itcard(flop1, &p.hand[3]);
-		      itcard(flop2, &p.hand[4]);
-		      itcard(flop0, &d.hand[2]);
-		      itcard(flop1, &d.hand[3]);
-		      itcard(flop2, &d.hand[4]);
-		      p.hand[2].whos_card = BOARDS;
-		      p.hand[3].whos_card = BOARDS;
-		      p.hand[4].whos_card = BOARDS;
-		      d.hand[2].whos_card = BOARDS;
-		      d.hand[3].whos_card = BOARDS;
-		      d.hand[4].whos_card = BOARDS;
+		      /* // Flop */
+		      /* itcard(flop0, &p.hand[2]); */
+		      /* itcard(flop1, &p.hand[3]); */
+		      /* itcard(flop2, &p.hand[4]); */
+		      /* itcard(flop0, &d.hand[2]); */
+		      /* itcard(flop1, &d.hand[3]); */
+		      /* itcard(flop2, &d.hand[4]); */
+		      /* p.hand[2].whos_card = BOARDS; */
+		      /* p.hand[3].whos_card = BOARDS; */
+		      /* p.hand[4].whos_card = BOARDS; */
+		      /* d.hand[2].whos_card = BOARDS; */
+		      /* d.hand[3].whos_card = BOARDS; */
+		      /* d.hand[4].whos_card = BOARDS; */
 		      
-		      // Get Payout -- BET 2x
-		      QueryBinArray(&p, state, is_my_card, PLAYERS, 5);
+		      /* // Get Payout -- BET 2x */
+		      /* QueryBinArray(&p, state, is_my_card, PLAYERS, 5); */
 
-		      // Turn & River
-		      itcard(turn, &p.hand[5]);
-		      itcard(river, &p.hand[6]);
-		      itcard(turn, &d.hand[5]);
-		      itcard(river, &d.hand[6]);
-		      p.hand[5].whos_card = BOARDS;
-		      p.hand[6].whos_card = BOARDS;
-		      d.hand[5].whos_card = BOARDS;
-		      d.hand[6].whos_card = BOARDS;
+		      /* // Turn & River */
+		      /* itcard(turn, &p.hand[5]); */
+		      /* itcard(river, &p.hand[6]); */
+		      /* itcard(turn, &d.hand[5]); */
+		      /* itcard(river, &d.hand[6]); */
+		      /* p.hand[5].whos_card = BOARDS; */
+		      /* p.hand[6].whos_card = BOARDS; */
+		      /* d.hand[5].whos_card = BOARDS; */
+		      /* d.hand[6].whos_card = BOARDS; */
 		      
-		      // Get Payouts
-		      twoxpay = Pays(&p, &d, 2.0, 1.0, 1.0);
-		      onexpay = Pays(&p, &d, 1.0, 1.0, 1.0);
-		      twoxbet_pay[stateToIndex(state)] += twoxpay;
-		      QueryBinArray(&p, state, is_my_card, PLAYERS, 7);
-		      onexbet_pay[stateToIndex(state)] += onexpay;
+		      /* // Get Payouts */
+		      /* twoxpay = Pays(&p, &d, 2.0, 1.0, 1.0); */
+		      /* onexpay = Pays(&p, &d, 1.0, 1.0, 1.0); */
+		      /* twoxbet_pay[stateToIndex(state)] += twoxpay; */
+		      /* QueryBinArray(&p, state, is_my_card, PLAYERS, 7); */
+		      /* onexbet_pay[stateToIndex(state)] += onexpay; */
 		      
-		      // Debugging Delete Me
-		      /* float test = Pays(&p, &d, 2.0, 1.0, 1.0); */
-		      /* printf("Two Bet: %f\n", test); */
-		      /* printf("Two Bet: %f\nOne Bet: %f\n", twoxbet_pay[stateToIndex(state)], onexbet_pay[stateToIndex(state)]); */
-		      // END Debugging
+		      /* // Debugging Delete Me */
+		      /* /\* float test = Pays(&p, &d, 2.0, 1.0, 1.0); *\/ */
+		      /* /\* printf("Two Bet: %f\n", test); *\/ */
+		      /* /\* printf("Two Bet: %f\nOne Bet: %f\n", twoxbet_pay[stateToIndex(state)], onexbet_pay[stateToIndex(state)]); *\/ */
+		      /* // END Debugging */
 
-		      // Clean Up
-		      reset_bin(&p.bin);
-		      reset_bin(&d.bin);
+		      /* // Clean Up */
+		      /* reset_bin(&p.bin); */
+		      /* reset_bin(&d.bin); */
 
 		    } // end giant if
 		  } // end river
