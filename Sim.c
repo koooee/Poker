78d7de636d5739d2a74f6e10082b2dd59ab2ae7e
Nick Kolegraff <nick@winthropgaming.com>
2012-05-29 06:00:25 -0700

    added random simulations

--- Sim.c
+++ Sim.c
@@ -10,7 +10,7 @@
 void Simulate();
 void jtest();
 void itcard(int icard, CARD *c);
-
+void RandomSimulation(long long num_times);
 int main(int argc, char *argv[])
 {
   PLAYER p;
@@ -26,7 +26,8 @@ int main(int argc, char *argv[])
   /* test_factorial(); */
   /* test_choose(); */
   /* test_largestv(); */
-  test_combinadic();
+  /* test_combinadic(); */
+  RandomSimulation(100LL);
 }
 
 void itcard(int icard, CARD *c)
@@ -195,3 +196,116 @@ void Simulate()
   printf("Total Combinations: %lld\n", totalCombin);
 }
 
+CARD *random_hand(){
+  long long *out;
+  combinadic(52,9,rand()%choose(52,9), out);
+  int i;
+  CARD *h;
+  for(i = 0; i < 9; i++){
+    /* FIXME we are casing a long long to int, will break for large games */
+    itcard((int)out[i], &h[i]);
+  }
+  return h;
+}
+
+void RandomSimulation(long long num_times){
+  PLAYER p;
+  PLAYER d;
+  unsigned long long totalCombin = 0;
+  int decksize = 52;
+  // Pay outs for 9 different states
+  float twoxbet_pay[9];
+  float onexbet_pay[9];
+  float twoxpay, onexpay;
+  int i;
+
+  for(i = 0; i < 9; i++)
+    {
+      twoxbet_pay[i] = 0.0;
+      onexbet_pay[i] = 0.0;
+    }
+
+  int action, holep0, holep1, holed0, holed1, flop0, flop1, flop2, turn, river;
+  InitPlayer(&p);
+  InitPlayer(&d);
+  char *state = malloc(2 * sizeof(char));
+  char *is_my_card;
+  long long s;
+  for(s == 0LL; s < num_times; s++){
+    /*  There are four actions described below */
+    /*  Bet 4x                  0 */
+    /*  Check First, Bet 2x     1 */
+    /*  Check, Check, Bet 1x    2 */
+    /*  Check, Check, Fold      3 */
+
+    /* Generate a Random Hand */
+    CARD *rhand = random_hand();
+    /*  Players Hole Cards */
+    p.hand[0] = rhand[0];
+    p.hand[1] = rhand[1];
+    p.hand[0].whos_card = PLAYERS;
+    p.hand[1].whos_card = PLAYERS;
+			  
+    /*  Dealers Hole Cards */
+    d.hand[0] = rhand[2];
+    d.hand[1] = rhand[3];
+    d.hand[0].whos_card = DEALERS;
+    d.hand[1].whos_card = DEALERS;
+
+    /*  Flop */
+    p.hand[2] = rhand[4];
+    p.hand[3] = rhand[5];
+    p.hand[4] = rhand[6];
+    d.hand[2] = rhand[4];
+    d.hand[3] = rhand[5];
+    d.hand[4] = rhand[6];
+
+    p.hand[2].whos_card = BOARDS;
+    p.hand[3].whos_card = BOARDS;
+    p.hand[4].whos_card = BOARDS;
+    d.hand[2].whos_card = BOARDS;
+    d.hand[3].whos_card = BOARDS;
+    d.hand[4].whos_card = BOARDS;
+		      
+    /*  Get Payout -- BET 2x */
+    /* QueryBinArray(&p, state, is_my_card, PLAYERS, 5); */
+
+    /*  Turn & River */
+    p.hand[5] = rhand[7];
+    p.hand[6] = rhand[8];
+    d.hand[5] = rhand[7];
+    d.hand[6] = rhand[8];
+    p.hand[5].whos_card = BOARDS;
+    p.hand[6].whos_card = BOARDS;
+    d.hand[5].whos_card = BOARDS;
+    d.hand[6].whos_card = BOARDS;
+		      
+    /*  Get Payouts */
+    twoxpay = Pays(&p, &d, 2.0, 1.0, 1.0);
+    onexpay = Pays(&p, &d, 1.0, 1.0, 1.0);
+    twoxbet_pay[stateToIndex(state)] += twoxpay;
+    QueryBinArray(&p, state, is_my_card, PLAYERS, 7);
+    onexbet_pay[stateToIndex(state)] += onexpay;
+		      
+    /*  Debugging Delete Me */
+    /* /\* float test = Pays(&p, &d, 2.0, 1.0, 1.0); *\/ */
+    /* /\* printf("Two Bet: %f\n", test); *\/ */
+    /* /\* printf("Two Bet: %f\nOne Bet: %f\n", twoxbet_pay[stateToIndex(state)], onexbet_pay[stateToIndex(state)]); *\/ */
+    /*  END Debugging */
+
+    /* Clean Up */
+    reset_bin(&p.bin);
+    reset_bin(&d.bin);
+  }
+  
+  /* Output Results */
+  int idx = 0;
+  for(idx = 0; idx < 9; idx++)
+    {
+      printf("2x State: %d Payout: %f\n", idx, twoxbet_pay[idx]);
+      printf("1x State: %d Payout: %f\n", idx, onexbet_pay[idx]);
+    }
+
+  printf("Total Iterations: %lld\n", num_times);
+
+}
